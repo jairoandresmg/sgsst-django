e923ad1259d6cf87df8d7b215e3bf12d71302550
jairoandresmg <128170079+jairoandresmg@users.noreply.github.com>
2025-08-14 16:15:16 -0500

    Update urls.py

--- sgsst/urls.py
+++ sgsst/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home.views import home
 
 urlpatterns = [
+    path("", home, name="home"),                     # portada
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
+    path("indicadores/", include("indicadores.urls"))  # monta las rutas de la app
 ]
