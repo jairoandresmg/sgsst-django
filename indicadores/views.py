cfad4c7e1fcbc979c2f0ad80afe8bf0ced1e421a
jairoandresmg <128170079+jairoandresmg@users.noreply.github.com>
2025-08-14 16:24:36 -0500

    Update views.py

--- indicadores/views.py
+++ indicadores/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroMensualForm
 from .models import RegistroMensual
 
+def lista_registros(request):
+    qs = RegistroMensual.objects.all()
+    return render(request, "indicadores/lista.html", {"items": qs})
+
 def nuevo_registro(request):
     if request.method == "POST":
         form = RegistroMensualForm(request.POST)
@@ -15,7 +19,3 @@ def nuevo_registro(request):
 def detalle_registro(request, pk):
     obj = get_object_or_404(RegistroMensual, pk=pk)
     return render(request, "indicadores/detalle.html", {"m": obj, "ind": obj.indicadores()})
-
-def lista_registros(request):
-    qs = RegistroMensual.objects.all()
-    return render(request, "indicadores/lista.html", {"items": qs})
